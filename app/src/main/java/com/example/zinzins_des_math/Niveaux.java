8d7a32c70e625a392c936ce8f8256b018d5a8494
Albi <albi.grainca@uha.fr>
2022-06-08 08:35:04 +0200

    reparation de l'appli

---
+++ app/src/main/java/com/example/zinzins_des_math/Niveaux.java
@@ -0,0 +1,14 @@
+package com.example.zinzins_des_math;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Niveaux extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_niveaux);
+    }
+}
