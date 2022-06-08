30ed894aea6b7c3fd6f4354f42fc2c38ed13f697
jerem <jeremy-2000@hotmail.fr>
2022-06-08 09:25:27 +0200

    changement nom fichier

--- app/src/main/java/com/example/zinzins_des_math/DifficultyActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/DifficultyActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Niveaux extends AppCompatActivity {
+public class DifficultyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
