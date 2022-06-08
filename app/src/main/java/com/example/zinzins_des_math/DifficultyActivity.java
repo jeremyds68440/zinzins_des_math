4135399f9feee7df856c3528aa26ec487e0d8103
jerem <jeremy-2000@hotmail.fr>
2022-06-08 10:50:42 +0200

    ajout transition

--- app/src/main/java/com/example/zinzins_des_math/DifficultyActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/DifficultyActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 
 public class DifficultyActivity extends AppCompatActivity {
 
-    ImageView back;
+    ImageView back,easy,medium,hard,IA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,50 @@ public class DifficultyActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent main = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(main);
+                finish();
+            }
+        });
+
+        easy = findViewById(R.id.facile);
+
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Facile = new Intent(getApplicationContext(), LevelActivity.class);
+                startActivity(Facile);
+                finish();
+            }
+        });
+
+        medium = findViewById(R.id.moyen);
+
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent moyen = new Intent(getApplicationContext(), LevelActivity.class);
+                startActivity(moyen);
+                finish();
+            }
+        });
+
+        hard = findViewById(R.id.difficile);
+
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent difficile = new Intent(getApplicationContext(), LevelActivity.class);
+                startActivity(difficile);
+                finish();
+            }
+        });
+
+        IA = findViewById(R.id.evolution);
+
+        IA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent evolution  = new Intent(getApplicationContext(), LevelActivity.class);
+                startActivity(evolution);
                 finish();
             }
         });
