8441e55a307648c43c4e2b7946fa3365aab1a2ea
jerem <jeremy-2000@hotmail.fr>
2022-06-08 15:07:39 +0200

    bouton parametre et jouer appui

--- app/src/main/java/com/example/zinzins_des_math/MainActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/MainActivity.java
@@ -2,23 +2,30 @@ package com.example.zinzins_des_math;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ImageView jouer = findViewById(R.id.jouer);
 
         jouer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                jouer.setColorFilter(Color.argb(80, 0, 0, 0));
                 Intent difficulty = new Intent(getApplicationContext(), DifficultyActivity.class);
                 startActivity(difficulty);
                 finish();
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         Parameter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Parameter.setColorFilter(Color.argb(80, 0, 0, 0));
                 Intent menuParametre = new Intent(getApplicationContext(), ParametreActivity.class);
                 startActivity(menuParametre);
                 finish();
