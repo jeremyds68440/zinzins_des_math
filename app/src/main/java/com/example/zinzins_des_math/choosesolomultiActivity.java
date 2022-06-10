0e7bfaf1a9594295963fb1102fe360254048fd8f
jerem <jeremy-2000@hotmail.fr>
2022-06-10 15:10:18 +0200

    ajout activity choix

---
+++ app/src/main/java/com/example/zinzins_des_math/choosesolomultiActivity.java
@@ -0,0 +1,14 @@
+package com.example.zinzins_des_math;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class choosesolomultiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_solo_or_multi);
+    }
+}
