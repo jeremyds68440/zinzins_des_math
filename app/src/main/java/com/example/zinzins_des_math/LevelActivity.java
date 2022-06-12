92d0255fc4b995678df5dbff98799a3c60719a4e
Albi <albi.grainca@uha.fr>
2022-06-12 17:19:44 +0200

    Ajout du nouveaux menu avec scroll pour les différents level

--- app/src/main/java/com/example/zinzins_des_math/LevelActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/LevelActivity.java
@@ -2,13 +2,64 @@ package com.example.zinzins_des_math;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class LevelActivity extends AppCompatActivity {
+
+    private ImageView back_diff;
+    private Button win;
+    private ScrollView scroll;
+    private LevelActivity activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level);
+
+        this.back_diff = findViewById(R.id.back_diffAct);
+        actionClickImage(this.back_diff, DifficultyActivity.class);
+
+        this.activity = this;
+        this.win = findViewById(R.id.reussi);
+        this.win.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder myPopup = new AlertDialog.Builder(activity);
+                myPopup.setTitle("YES");
+                myPopup.setMessage("J'ai reussi");
+                myPopup.show();
+            }
+        });
+
+
+        scroll = findViewById(R.id.scroll);
+        scroll.post(new Runnable() {
+            @Override
+            public void run() {
+                scroll.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
+    }
+
+    public void actionClickImage(ImageView button, Class act){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setColorFilter(Color.argb(80, 0, 0, 0));
+                Intent intent = new Intent(getApplicationContext(), act);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        });
     }
 }
