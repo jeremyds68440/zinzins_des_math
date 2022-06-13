200a8703e770032a7d8522e1b6054ea087bc5636
Kinamlem <103247065+Kinamlem@users.noreply.github.com>
2022-06-13 15:03:18 +0200

    Fin de la Leaderboard

--- app/src/main/java/com/example/zinzins_des_math/LeaderboardActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/LeaderboardActivity.java
@@ -5,7 +5,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,6 +24,7 @@ import java.util.ArrayList;
 
 public class LeaderboardActivity extends AppCompatActivity {
 
+    ImageView leadMultifactor,leadMathemaquizz, leadRoulette,leadFacile,leadMoyen,leadDifficile;
     DatabaseReference mDatabase;
     ListView listView;
     ArrayList<String> arrayList = new ArrayList<>();
@@ -28,12 +32,92 @@ public class LeaderboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
         mDatabase = (DatabaseReference) FirebaseDatabase.getInstance().getReference("users");
-        Query sortedDatabase = mDatabase.orderByChild("scoreMathemaquizzFacile");
         listView = (ListView) findViewById(R.id.leaderboard);
-        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        leadMultifactor = (ImageView) findViewById(R.id.leadMultifactor);
+        leadMathemaquizz = (ImageView) findViewById(R.id.leadMathemaquizz);
+        leadRoulette = (ImageView) findViewById(R.id.leadRoulette);
+        leadFacile = (ImageView) findViewById(R.id.leadFacile);
+        leadMoyen = (ImageView) findViewById(R.id.leadMoyen);
+        leadDifficile = (ImageView) findViewById(R.id.leadDifficile);
+        leadMultifactor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leadFacile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showFacile("Multifactor", 5);
+                    }
+                });
+                leadMoyen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMoyen("Multifactor", 6);
+                    }
+                });
+                leadDifficile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDifficile("Multifactor", 4);
+                    }
+                });
+            }
+        });
+        leadMathemaquizz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leadFacile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showFacile("Mathemaquizz", 2);
+                    }
+                });
+                leadMoyen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMoyen("Mathemaquizz", 3);
+                    }
+                });
+                leadDifficile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDifficile("Mathemaquizz", 1);
+                    }
+                });
+            }
+        });
+        leadRoulette.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                leadFacile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showFacile("Roulette", 8);
+                    }
+                });
+                leadMoyen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showMoyen("Roulette", 9);
+                    }
+                });
+                leadDifficile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDifficile("Roulette", 7);
+                    }
+                });
+            }
+        });
+    }
+
+    public void showFacile(String game, int dbNumber){
+        arrayList.clear();
+        Query sortedDatabase = mDatabase.orderByChild("score"+game+"Facile");
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(arrayAdapter);
         sortedDatabase.addChildEventListener(new ChildEventListener() {
             @Override
@@ -41,30 +125,82 @@ public class LeaderboardActivity extends AppCompatActivity {
                 final User utilisateur = new User();
                 final Field[] fields = utilisateur.getClass().getDeclaredFields();
                 String username = (String) dataSnapshot.child(fields[10].getName()).getValue();
-                String value =  Long.toString((long) dataSnapshot.child(fields[2].getName()).getValue());
+                String value =  Long.toString((long) dataSnapshot.child(fields[dbNumber].getName()).getValue());
                 arrayList.add(username +" : "+ value);
                 arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
 
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    public void showMoyen(String game, int dbNumber){
+        arrayList.clear();
+        Query sortedDatabase = mDatabase.orderByChild("score"+game+"Moyen");
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        sortedDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+                final User utilisateur = new User();
+                final Field[] fields = utilisateur.getClass().getDeclaredFields();
+                String username = (String) dataSnapshot.child(fields[10].getName()).getValue();
+                String value =  Long.toString((long) dataSnapshot.child(fields[dbNumber].getName()).getValue());
+                arrayList.add(username +" : "+ value);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
 
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    public void showDifficile(String game, int dbNumber){
+        arrayList.clear();
+        Query sortedDatabase = mDatabase.orderByChild("score"+game+"Difficile");
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        sortedDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
+                final User utilisateur = new User();
+                final Field[] fields = utilisateur.getClass().getDeclaredFields();
+                String username = (String) dataSnapshot.child(fields[10].getName()).getValue();
+                String value =  Long.toString((long) dataSnapshot.child(fields[dbNumber].getName()).getValue());
+                arrayList.add(username +" : "+ value);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {
-
-            }
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
 
-            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String previousChildName) {}
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
         });
     }
 }
