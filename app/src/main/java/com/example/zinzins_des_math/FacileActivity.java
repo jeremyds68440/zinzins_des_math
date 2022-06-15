9f13bc700ec212ddc085ecad828251920a479f2a
Albi <albi.grainca@uha.fr>
2022-06-15 14:17:20 +0200

    rajout de popup info

--- app/src/main/java/com/example/zinzins_des_math/FacileActivity.java
+++ app/src/main/java/com/example/zinzins_des_math/FacileActivity.java
@@ -2,18 +2,28 @@ package com.example.zinzins_des_math;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FacileActivity extends AppCompatActivity {
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String ETAT_SOUND_THEME = "etat_sound_theme";
     public static final String ETAT_SOUND_EFFECT = "etat_sound_effect";
+
+    private TextView difficultyString;
 
     private boolean sound_theme_state;
     private boolean sound_effect_state;
@@ -42,6 +52,14 @@ public class FacileActivity extends AppCompatActivity {
         actionClickImagefacile(mathemaquizz, MathemaQuizzActivity.class,0);
         actionClickImagefacile(roulette, RouletteActivity.class,0);
 
+        ImageView infoMultifactor = findViewById(R.id.info_multifactor_facile);
+        ImageView infoMathemaquizz = findViewById(R.id.info_mathemaquizz_facile);
+        ImageView infoRoulette = findViewById(R.id.info_roulette_facile);
+
+        actionClickInfo(infoMultifactor, "multifactor");
+        actionClickInfo(infoMathemaquizz, "mathemaquizz");
+        actionClickInfo(infoRoulette, "roulette");
+
     }
 
     private void actionClickImagefacile(ImageView button, Class act, int difficulty) {
@@ -61,10 +79,44 @@ public class FacileActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void actionClickInfo(ImageView button, String gameName) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setColorFilter(Color.argb(80, 0, 0, 0));
+                createPopupInfo(gameName);
+            }
+        });
+    }
     public void onBackPressed() {
         Intent intent = new Intent(getApplicationContext(), DifficultyActivity.class);
         startActivity(intent);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         finish();
+    }
+
+    private void createPopupInfo(String gameName){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.MyDialogTheme);
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        View dialogView_back = LayoutInflater.from(this).inflate(R.layout.custom_popup_info, viewGroup, false);
+        builder.setView(dialogView_back);
+        AlertDialog alertDialog = builder.create();
+
+        Button close = dialogView_back.findViewById(R.id.button_cancel);
+        LinearLayout popup= dialogView_back.findViewById((R.id.layout_popup_info));
+
+        String imagePopup = gameName + "_info";
+        int resId = getResources().getIdentifier(imagePopup, "drawable", getPackageName());
+        popup.setBackground(getDrawable(resId));
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.show();
+
     }
 }
